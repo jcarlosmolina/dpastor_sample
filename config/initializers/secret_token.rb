be9e2e9b15983ca88baa26f4c1a6eced5c74e17b
Juan Carlos Molina <jcarlosmolina@gmail.com>
2013-04-03 14:53:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DpastorSample::Application.config.secret_token = 'cd0750fe009607e2bf1083317c8cb6a898b542b79a9768292c4d6d6c4dbcc10b2d55bd51246a81ea480d52be963c69c0a4fa64fd954325cd955d2ee1a127d5dd'
